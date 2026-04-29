239b0fc5dd21800d901b10d37901ececb6e4020b
Subhashini Konda <249x5a05u8le@gmail.com>
2026-04-29 10:22:18 +0530

    Time: 7 ms (30.43%), Space: 47 MB (28.81%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> st=new HashSet<>();
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                char c=board[i][j];
+                if(c != '.'){
+                    String rk="r"+i+"-"+c;
+                    String ck="c"+j+"-"+c;
+                    String bk="b"+((i/3) * 3+(j/3))+"-"+c;
+                    if(st.contains(rk)|| st.contains(ck)|| st.contains(bk)){
+                        return false;
+                    }else{
+                        st.add(rk);
+                        st.add(ck);
+                        st.add(bk);
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
