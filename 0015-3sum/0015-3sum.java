aff49240858898f84f5415e4cea7855418e56108
Subhashini Konda <249x5a05u8le@gmail.com>
2026-05-05 18:31:35 +0530

    Time: 33 ms (73.98%), Space: 60.3 MB (9.75%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,32 +1,35 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> l=new ArrayList<>();
+        //using Pointers
+        List<List<Integer>> al=new ArrayList<>();
         for(int i=0;i<nums.length-2;i++){
-            if(i>0 && nums[i]==nums[i-1])
-                continue;
             int p1=i+1,p2=nums.length-1;
-            int tar=-1*nums[i];
+            int t=-1 *nums[i];
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
             while(p1<p2){
-                if(nums[p1]+nums[p2]==tar){
-                    ArrayList<Integer> al=new ArrayList<>();
-                    al.add(nums[i]);
-                    al.add(nums[p1]);
-                    al.add(nums[p2]);
-                    l.add(al);
-                    while(p1<p2 && nums[p1]==nums[p1+1])
+                if(nums[p1]+nums[p2]==t){
+                    List<Integer> l=new ArrayList<>();
+                    l.add(nums[i]);
+                    l.add(nums[p1]);
+                    l.add(nums[p2]);
+                    al.add(l); 
+                    while(p1<nums.length-1 && nums[p1]==nums[p1+1]){
                         p1++;
-                    while(p1<p2 && nums[p2]==nums[p2-1])
+                    }
+                    while(p2>0 && nums[p2]==nums[p2-1]){
                         p2--;
-                    p1++;
-                    p2--;
-                }else if(nums[p1]+nums[p2]<tar){
+                    }  
+                    p1++;p2--;
+                }else if(nums[p1]+nums[p2]<t){
                     p1++;
                 }else{
                     p2--;
                 }
             }
         }
-        return l;
+        return al;
     }
 }
