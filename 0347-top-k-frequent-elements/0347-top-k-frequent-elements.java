71c8ab8c6e83db6379fc49bed71f1b4a85c184df
Subhashini Konda <249x5a05u8le@gmail.com>
2026-05-08 11:02:09 +0530

    Time: 15 ms (54.96%), Space: 47.9 MB (19.31%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -4,12 +4,15 @@ class Solution {
         for(int i:nums){
             hm.put(i,hm.getOrDefault(i,0)+1);
         }
-        PriorityQueue<Integer> pq=new PriorityQueue((a,b)->hm.get(b)-hm.get(a));
+        PriorityQueue<Integer> pq=new PriorityQueue((a,b)->hm.get(a)-hm.get(b));
         for(int n:hm.keySet()){
             pq.offer(n);
+            if(pq.size()>k){
+                pq.poll();
+            }
         }
         int[] res=new int[k];
-        for(int i=0;i<k;i++){
+        for(int i=k-1;i>=0;i--){
             res[i]=pq.poll();
         }
         return res;
